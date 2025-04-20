11057101f10a674a2091286f77de14f2acb1c96d
Rishabh Munjal <rishabh5566ind1111@gmail.com>
2025-04-20 22:17:06 +0530

    erh

--- backend/app.js
+++ backend/app.js
@@ -60,6 +60,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
+app.listen(port , '0.0.0.0', () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
